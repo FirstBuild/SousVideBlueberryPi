de96ce286a7c873e0c03e050225fbe4e021513ea
mylescaley <myles.caley@gmail.com>
2015-03-12 13:52:37 +0000

    removed sudo from cooktop script

--- cooktop.sh
+++ cooktop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo hciconfig hci0 reset;
-sudo hciconfig hci1 reset;
-sudo BLENO_HCI_DEVICE_ID=0 NOBLE_HCI_DEVICE_ID=1 node index.js
+hciconfig hci0 up;
+hciconfig hci1 up;
+hciconfig hci0 reset;
+hciconfig hci1 reset;
+BLENO_HCI_DEVICE_ID=0 NOBLE_HCI_DEVICE_ID=1 node index.js
